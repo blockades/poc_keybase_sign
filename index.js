b6832de8886c493b2fa2b2023197adc2f7045348
Mathieu d'Alexis <mathieu@atnos.com>
2017-11-27 03:37:04 +0100

    Add comments

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const {encrypt, decrypt} = require('triplesec');
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-function hex2bin(hex)
-{
-  return new Buffer(hex, "hex");
-}
-
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600});
@@ -65,13 +60,17 @@ ipcMain.on('form-poc', function (event, data) {
   var username = data.username;
   var password = data.password;
   var content = data.content;
+
+  // Login to keybase.io with form credentials
   login({username : username, passphrase : password}, function(err, res) {
+    // Keep cookies to use curret session
     const cookies = res.cookies;
     var j = request.jar();
     for (var cookie of cookies) {
       j.setCookie(request.cookie(cookie), 'https://keybase.io');
 
     }
+    // Look up for username in keybase user list
     request({
       url: 'https://keybase.io/_/api/1.0/user/lookup.json',
       qs: {usernames: username},
@@ -80,19 +79,25 @@ ipcMain.on('form-poc', function (event, data) {
       body = JSON.parse(body);
       key_id = body.them[0].public_keys.primary.kid;
 
+      // Fetch the signing key (ops: 0x08)
       request({
         url: 'https://keybase.io/_/api/1.0/key/fetch.json',
         qs: {kids: key_id, cookies: cookies, ops: 8},
         jar: j
       }, function (err, res, body) {
+        // The key is sent in P3SKB so we have to Base64 decode it and decrypt it using triplesec
         body = JSON.parse(body);
         var priv_key = body.keys[0].bundle;
-        buffer = Buffer.from(priv_key, 'base64');
-        obj = purepack.unpack(buffer);
-        enc_key = obj.body.priv.data;
-        key = decrypt({ key: Buffer.from(password, 'utf8'), data : enc_key }, function(err, key) {
-          console.log(err);
+        var buffer = Buffer.from(priv_key, 'base64');
+        var obj = purepack.unpack(buffer);
+        var enc_key = obj.body.priv.data;
+        var key = decrypt({
+          key: Buffer.from(password, 'utf8'),
+          data : enc_key
+        }, function(err, key) {
+          // Let's add PGP KEY BLOCK header and tailer
           key = encode({header: {comment : "none", version : "0" } }, "PRIVATE KEY BLOCK", key);
+          // Create our KeyManager for user
           kbpgp.KeyManager.import_from_armored_pgp({
             armored: key
           }, function(err, user) {
@@ -111,19 +116,20 @@ ipcMain.on('form-poc', function (event, data) {
                 });
               } else {
                 console.log("Loaded private key w/o passphrase");
-                kbpgp.box({
-                  msg: content,
-                  sign_with: user
-                }, function (err, result_string, result_buf) {
-                  console.log(err, result_string);
-                  event.sender.send('signed-msg', {s_msg: result_string});
-                });
               }
             }
             else {
               console.log('error', err);
               return;
             }
+            // If import succeeded sign content with user key
+            kbpgp.box({
+              msg: content,
+              sign_with: user
+            }, function (err, result_string, result_buf) {
+              console.log(err, result_string);
+              event.sender.send('signed-msg', {s_msg: result_string});
+            });
           });
         });
       });
